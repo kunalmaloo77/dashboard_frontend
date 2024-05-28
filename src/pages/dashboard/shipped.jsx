10cf6fe0e9f2bb03c47ef222b85141088f3b0539
Kunal Maloo <kunnalmaloo@gmail.com>
2024-05-28 11:50:12 +0530

    added pagination in rto

--- src/pages/dashboard/shipped.jsx
+++ src/pages/dashboard/shipped.jsx
@@ -47,19 +47,6 @@ export function Shipped() {
             <Route key={path} exact path={path} element={element} />
           ))}
         </Routes>
-        <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Typography variant="small" color="blue-gray" className="font-normal">
-            Page 1 of 10
-          </Typography>
-          <div className="flex gap-2">
-            <Button variant="outlined" size="sm">
-              Previous
-            </Button>
-            <Button variant="outlined" size="sm">
-              Next
-            </Button>
-          </div>
-        </CardFooter>
       </Card>
     </div>
   );
