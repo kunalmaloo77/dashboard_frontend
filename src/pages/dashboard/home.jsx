7f6fc0cece2a87d563bf80211c8dfc0ea31d19c8
Kunal Maloo <kunnalmaloo@gmail.com>
2024-07-19 15:27:51 +0530

    channel wise chart made

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -4,6 +4,10 @@ import {
   Card,
   CardHeader,
   CardBody,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Radio,
 } from "@material-tailwind/react";
 import {
   ArrowUpIcon,
@@ -26,12 +30,12 @@ export function Home() {
   const [delivered, setDelivered] = useState(0);
   const [returned, setReturned] = useState(0);
 
-  const getData = async (startDate, endDate) => {
+  const getData = async (startDate, endDate, type) => {
     try {
       let returnedOrder = 0;
       let deliveredOrder = 0;
       setLoading(true);
-      const res = await axiosPublic.get('/clients/api/date', {
+      const res = await axiosPublic.get(`/clients/api/date/${type}`, {
         params: { startDate: startDate, endDate: endDate }
       });
       const temp = Object.values(res.data.reduce((result, currentValue) => {
@@ -59,6 +63,20 @@ export function Home() {
     }
   }
 
+  const handleSelectionChange = async (e) => {
+    const selectedValue = e.target.value
+    console.log(`Request triggered for: ${selectedValue}`);
+    if (selectedValue === 'shopify') {
+      getData(dates[0], dates[1], "shopify");
+    } else if (selectedValue === 'dealshunter') {
+      getData(dates[0], dates[1], "dealshunter");
+    } else if (selectedValue === 'pagazo') {
+      getData(dates[0], dates[1], "pagazo");
+    } else {
+      getData(dates[0], dates[1], "all");
+    }
+  }
+
   useEffect(() => {
     const today = new Date();
     const sevenDaysFromToday = new Date(today);
@@ -72,7 +90,7 @@ export function Home() {
 
   useEffect(() => {
     if (dates.length === 2 && dates[0] instanceof Date && dates[1] instanceof Date) {
-      getData(dates[0], dates[1]);
+      getData(dates[0], dates[1], "all");
     }
   }, [dates]);
 
@@ -97,6 +115,116 @@ export function Home() {
         ))}
       </div>
       <div className="mb-6">
+        <Card className="w-full max-w-[40rem] mb-6">
+          <List className="flex-row">
+            <ListItem className="p-0">
+              <label
+                htmlFor="horizontal-list-all"
+                className="flex w-full cursor-pointer items-center px-3 py-2"
+              >
+                <ListItemPrefix className="mr-3">
+                  <Radio
+                    value="all"
+                    name="horizontal-list"
+                    id="horizontal-list-all"
+                    ripple={false}
+                    className="hover:before:opacity-0"
+                    onChange={handleSelectionChange}
+                    containerProps={{
+                      className: "p-0",
+                    }}
+                    defaultChecked={true}
+                  />
+                </ListItemPrefix>
+                <Typography
+                  color="blue-gray"
+                  className="font-medium text-blue-gray-400"
+                >
+                  All
+                </Typography>
+              </label>
+            </ListItem>
+            <ListItem className="p-0">
+              <label
+                htmlFor="horizontal-list-shopify"
+                className="flex w-full cursor-pointer items-center px-3 py-2"
+              >
+                <ListItemPrefix className="mr-3">
+                  <Radio
+                    name="horizontal-list"
+                    value="shopify"
+                    id="horizontal-list-shopify"
+                    ripple={false}
+                    onChange={handleSelectionChange}
+                    className="hover:before:opacity-0"
+                    containerProps={{
+                      className: "p-0",
+                    }}
+                  />
+                </ListItemPrefix>
+                <Typography
+                  color="blue-gray"
+                  className="font-medium text-blue-gray-400"
+                >
+                  Shopify
+                </Typography>
+              </label>
+            </ListItem>
+            <ListItem className="p-0">
+              <label
+                htmlFor="horizontal-list-dealshunter"
+                className="flex w-full cursor-pointer items-center px-3 py-2"
+              >
+                <ListItemPrefix className="mr-3">
+                  <Radio
+                    name="horizontal-list"
+                    id="horizontal-list-dealshunter"
+                    ripple={false}
+                    value="dealshunter"
+                    className="hover:before:opacity-0"
+                    onChange={handleSelectionChange}
+                    containerProps={{
+                      className: "p-0",
+                    }}
+                  />
+                </ListItemPrefix>
+                <Typography
+                  color="blue-gray"
+                  className="font-medium text-blue-gray-400"
+                >
+                  Deals Hunter
+                </Typography>
+              </label>
+            </ListItem>
+            <ListItem className="p-0">
+              <label
+                htmlFor="horizontal-list-pagazo"
+                className="flex w-full cursor-pointer items-center px-3 py-2"
+              >
+                <ListItemPrefix className="mr-3">
+                  <Radio
+                    name="horizontal-list"
+                    onChange={handleSelectionChange}
+                    id="horizontal-list-pagazo"
+                    ripple={false}
+                    value="pagazo"
+                    className="hover:before:opacity-0"
+                    containerProps={{
+                      className: "p-0",
+                    }}
+                  />
+                </ListItemPrefix>
+                <Typography
+                  color="blue-gray"
+                  className="font-medium text-blue-gray-400"
+                >
+                  Pagazo
+                </Typography>
+              </label>
+            </ListItem>
+          </List>
+        </Card>
+
         {
           loading ?
             <Loader />
